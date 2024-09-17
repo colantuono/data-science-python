aa4835ea7fa689c8c8b9f1417ce6e2556aef08ec
colantuono <pedrocolantuono@protonmail.com>
2024-09-17 12:07:06 -0300

    LR rus

--- classification/utils/modelling_tools.py
+++ classification/utils/modelling_tools.py
@@ -95,15 +95,38 @@ def plot_curva_pr(recall, precision, auc_pr, nome_modelo=''):
 
 def ks(dataset):
     '''calcula o KS score e o p-valor'''
-    nb_stat_ks, nb_ks_val = ks_2samp(
+    stat_ks, ks_val = ks_2samp(
         dataset[dataset['target']==0],
         dataset[dataset['target']==1]
     )
     
-    print(f'KS: {nb_stat_ks[1]:.4f}')
-    print(f'p-valor: {nb_ks_val[1]:.4f}')
+    print(f'KS: {stat_ks[1]:.4f}')
+    print(f'p-valor: {ks_val[1]:.4f}')
     
-    return nb_stat_ks, nb_ks_val
+    return stat_ks, ks_val
+
+def kstable(dataset, target=None, prob=None, print=False):
+    dataset['target0'] = 1 - dataset[target]
+    dataset['bucket'] = pd.qcut(dataset[prob], 10)
+    grouped = dataset.groupby('bucket', as_index=False)
+    kstable = pd.DataFrame()
+    kstable['min_prob'] = grouped.min()[prob]
+    kstable['max_prob'] = grouped.max()[prob]
+    kstable['events'] = grouped.sum()[target]
+    kstable['nonevents'] = grouped.sum()['target0']
+    kstable = kstable.sort_values(by='min_prob', ascending=False).reset_index(drop=True)
+    kstable['event_rate'] = (kstable['events'] / dataset[target].sum()).apply('{0:.2%}'.format)
+    kstable['nonevent_rate'] = (kstable['nonevents'] / dataset['target0'].sum()).apply('{0:.2%}'.format)
+    kstable['cum_event_rate'] = (kstable['events'] / dataset[target].sum()).cumsum()
+    kstable['cum_nonevent_rate'] = (kstable['nonevents'] / dataset['target0'].sum()).cumsum()
+    kstable['KS'] = np.round(kstable['cum_event_rate'] - kstable['cum_nonevent_rate'], 3) * 100 
+    
+    if print:
+        print(kstable)
+        
+    return kstable
+    
+ 
     
 def plot_separacao_classes(dataset, colunas=[''], titulos=[''], modelo=''):
     '''plota o gráfico de Sepração de Classes'''
@@ -230,3 +253,103 @@ def t_test_S(x0, x_hat, S, n, alpha=.5):
         print(f'O valor {x0:.4f} tem probabilidade de {p_value:.2%} de pertencer a validação por flutuação estatística, e com IC={1-alpha}, rejeito H0')
 
     return p_value
+
+def permutation_test(arr_model1, arr_model2, alpha=.5):
+    arr_model1 = np.array(arr_model1)
+    arr_model2 = np.array(arr_model2)
+
+    avg_arr1 = arr_model1.mean()
+    avg_arr2 = arr_model2.mean()
+
+    mean_diff = avg_arr1 - avg_arr2 
+
+    full_arr = np.concatenate([arr_model1, arr_model2])
+
+    mean_lst = []
+    
+    for i in range(10000):
+        avg1 = np.random.choice(full_arr, size=len(arr_model1), replace=True).mean()
+        avg2 = np.random.choice(full_arr, size=len(arr_model2), replace=True).mean()
+
+        mean_lst.append(avg1-avg2)
+        
+    if mean_diff > 0:
+        p_val = np.sum(np.array(mean_lst) >= mean_diff)/i
+    else:
+        p_val = np.sum(np.array(mean_lst) <= mean_diff)/i
+    
+    print(f'Diferença entre as médias: {avg1:.4f}-{avg2:.4f}={mean_diff:.4f}')
+    
+    if p_val > alpha:
+        print(f'Os modelos parecem produzir o mesmo resultado com IC={1-alpha} (não rejeito H0)')    
+    else:
+        print(f'Os modelos parecem produzir resultados diferentes com IC={1-alpha} (rejeito H0)')      
+    
+    return p_val, mean_lst, mean_diff
+
+def plot_pts(data, p_val, mean_diff, title=''):
+    ni, xi = np.histogram(data, bins=17)
+    
+    fi = ni/np.sum(ni)
+    
+    delta_xi = xi[2] - xi[1]
+    xi = xi + np.abs(delta_xi)/2
+    plt.plot([mean_diff, mean_diff], [0, np.max(ni)*0.85])
+    
+    if p_val <= .05:
+        alinhamento='center'
+    elif mean_diff < 0:
+        alinhamento='right'
+    else:
+        alinhamento='left'
+        
+    plt.text(mean_diff, np.max(ni)*.9, f'p_val={p_val:.3f}')
+    plt.bar(xi[:-1], ni, label='Data', edgecolor='k', color='g', width=np.abs(xi[2]-xi[1]*.9))
+    plt.title(f'Permutation Test - {title}')
+    plt.ylabel('Counts')
+    plt.ticklabel_format(axis='x', style='sci', scilimits=(-3,3));
+    return plt
+        
+def psi(score_init, score_new, num_bins=10, modo='fixo'):
+    eps = 1e-4
+    
+    ## prepara os bins
+    min_val = min(min(score_init), min(score_new))
+    max_val = max(max(score_init), max(score_new))
+    
+    if modo == 'fixo':
+        bins = [min_val + (max_val - min_val)*(i) / num_bins for i in range(num_bins+1) ]
+    elif modo == 'quantil':
+        bins = pd.qcut(score_init, q=num_bins, retbins=True)[1] ## cria quantis baseado na população inicial
+    else:
+        raise ValueError(f'Modo {modo} não reconhecido, deve ser {"fixo"} ou {"quantil"}')
+    
+    bins[0] = min_val - eps # corrige o limite inferior
+    bins[-1] = max_val + eps # corrige o limite superior
+    
+    ## bucketiza a população inicial e conta a amostra dentro de cada bucket
+    bins_init = pd.cut(score_init, bins=bins, labels=range(1, num_bins+1))
+    df_init = pd.DataFrame({'Inicial': score_init, 'bins': bins_init})
+    grp_init = df_init.groupby('bins').count()
+    grp_init['Percentual Inicial'] = grp_init['Inicial'] / sum(grp_init['Inicial'])
+  
+    ## bucketiza a nova população e conta a amostra dentro de cada bucket
+    bins_new = pd.cut(score_new, bins=bins, labels=range(1, num_bins+1))
+    df_new = pd.DataFrame({'Nova': score_new, 'bins': bins_new})
+    grp_new = df_new.groupby('bins').count()
+    grp_new['Percentual Nova'] = grp_new['Nova'] / sum(grp_new['Nova'])
+
+    ## compara os bins 
+    psi_df = grp_init.join(grp_new, on='bins', how='inner')
+    
+    # Adiciona um valor pequeno quando a porcentagem for 0
+    psi_df['Percentual Inicial'] = psi_df['Percentual Inicial'].apply(lambda x: eps if x == 0 else x)
+    psi_df['Percentual Nova'] = psi_df['Percentual Nova'].apply(lambda x: eps if x == 0 else x)
+    
+    ## calcula o PSI
+    psi_df['PSI'] = psi_df['Percentual Inicial'] - psi_df['Percentual Nova'] * np.log(psi_df['Percentual Inicial'] / psi_df['Percentual Nova'])
+    
+    return psi_df['PSI'].values
+
+    
+     
